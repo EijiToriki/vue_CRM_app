c2c0795aed5f737f4dcf3ea4da2a18128c680ef2
Eiji Toriki <ageForEngi@outlook.jp>
2024-05-02 21:30:49 +0900

    sec2完了

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,20 @@
+<script setup>
+import GuestLayout from '@/Layouts/Guest.vue'
+import Label from '@/Components/Label.vue';
+import Input from '@/Components/Input.vue';
+
+const emitTest = (e) => {
+  console.log(e)
+}
+</script>
+
+<template>
+  <GuestLayout>
+    <Label value="件名">タイトル</Label>
+    <Input 
+      model-value="初期値"
+      @update:model-value="emitTest"
+    >
+    </Input>
+  </GuestLayout>
+</template>
