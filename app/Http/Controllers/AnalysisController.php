117bc6fb959b04507e3bbd311fcb9ee38f33be2a
Eiji Toriki <ageForEngi@outlook.jp>
2024-05-23 14:07:34 +0900

    RFM Analysis Query

--- app/Http/Controllers/AnalysisController.php
+++ app/Http/Controllers/AnalysisController.php
@@ -10,6 +10,86 @@ use App\Models\Order;
 class AnalysisController extends Controller
 {
     public function index(){
+        $startDate = '2023-05-01';
+        $endDate = '2024-05-23';
+        // RFM分析
+        // 1. 購買ID毎にまとめる
+        $subQuery = Order::betweenDate($startDate, $endDate)
+            ->groupBy('id')
+            ->selectRaw('id, customer_id, customer_name, SUM(subtotal) as totalPerPurchase, created_at');
+        
+        // 2. 会員毎にまとめて購入金額順にソートする
+        $subQuery = DB::table($subQuery)
+            ->groupBy('customer_id')
+            ->selectRaw('customer_id, customer_name, 
+                max(created_at) as recentDate,
+                datediff(now(), max(created_at)) as recency,
+                count(customer_id) as frequency,
+                sum(totalPerPurchase) as monetary');
+        
+        // 4. 会員毎のRFMランクを計算
+        $subQuery = DB::table($subQuery)
+        ->selectRaw('customer_id, customer_name, 
+        recentDate, recency, frequency, monetary,
+        case
+            when recency < 14 then 5
+            when recency < 28 then 4
+            when recency < 60 then 3
+            when recency < 90 then 2
+            else 1 end as r,
+        case
+            when 7 <= frequency then 5
+            when 5 <= frequency then 4
+            when 3 <= frequency then 3
+            when 2 <= frequency then 2
+            else 1 end as f,
+        case
+            when 300000 <= monetary then 5
+            when 200000 <= monetary then 4
+            when 100000 <=  monetary then 3
+            when 30000 <= monetary then 2
+            else 1 end as m');
+
+        // 5. 会員毎のRFMランクを計算
+        $total = DB::table($subQuery)->count();
+
+        $rCount = DB::table($subQuery)
+        ->groupBy('r')
+        ->selectRaw('r, count(r)')
+        ->orderBy('r', 'desc')
+        ->get();
+
+        $fCount = DB::table($subQuery)
+        ->groupBy('f')
+        ->selectRaw('f, count(f)')
+        ->orderBy('f', 'desc')
+        ->get();
+
+        $mCount = DB::table($subQuery)
+        ->groupBy('m')
+        ->selectRaw('m, count(m)')
+        ->orderBy('m', 'desc')
+        ->get();
+
+        $data = DB::table($subQuery)
+            ->groupBy('r')
+            ->selectRaw('concat("r_", r) as rRank,
+            count(case when f=5 then 1 end) as f_5,
+            count(case when f=4 then 1 end) as f_4,
+            count(case when f=3 then 1 end) as f_3,
+            count(case when f=2 then 1 end) as f_2,
+            count(case when f=1 then 1 end) as f_1')
+            ->orderBy('rRank', 'desc')
+            ->get();
+
+        dd($data);
+
+
+        return Inertia::render('Analysis');
+    }
+
+    // デシル分析：記録用
+    public function decile(){
         $startDate = '2022-08-01';
         $endDate = '2022-08-31';
 
@@ -80,9 +160,7 @@ class AnalysisController extends Controller
                 ->get();
 
         
-        // dd($subQuery);
-        dd($data);
-
-        return Inertia::render('Analysis');
+        // // dd($subQuery);
+        // dd($data);
     }
 }
--- resources/js/Pages/Analysis.vue
+++ resources/js/Pages/Analysis.vue
@@ -5,6 +5,7 @@ import { Head } from '@inertiajs/inertia-vue3';
 import axios from 'axios';
 import { onMounted, reactive } from 'vue';
 import Chart from '@/Components/Chart.vue'
+import ResultTable from '@/Components/ResultTable.vue';
 
 const data = reactive({})
 
@@ -32,7 +33,7 @@ const getData = async() => {
             data.data = res.data.data
             data.labels = res.data.labels
             data.totals = res.data.totals
-            
+            data.type = res.data.type
         })
     }catch(e){
         console.log(e.message)
@@ -63,7 +64,9 @@ const getData = async() => {
                             <input type="radio" v-model="form.type" value="perMonth">
                             <span class="mr-4">月別</span>
                             <input type="radio" v-model="form.type" value="perYear">
-                            <span class="mr-4">年別</span><br>
+                            <span class="mr-4">年別</span>
+                            <input type="radio" v-model="form.type" value="decile">
+                            <span class="mr-4">デシル分析</span><br>
                             From:<input type="date" name="startDate" v-model="form.startDate">
                             To:<input type="date" name="endDate" v-model="form.endDate"><br>
                             <button class="flex mx-auto text-white bg-indigo-500 border-0 mt-4 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">分析する</button>
@@ -71,24 +74,8 @@ const getData = async() => {
 
                         <div v-show="data.data">
                             <Chart :data="data" />
+                            <ResultTable :data="data" />
                         </div>
-
-                        <div v-show="data.data" class="lg:w-2/3 w-full mx-auto overflow-auto">
-                            <table class="table-auto w-full text-left whitespace-no-wrap">
-                              <thead>
-                                <tr>
-                                  <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">年月日</th>
-                                  <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">金額</th>
-                                </tr>
-                              </thead>
-                              <tbody>
-                                <tr v-for="item in data.data" :key="item.date">
-                                  <td class="border-b-2 border-gray-200 px-4 py-3">{{ item.date }}</td>
-                                  <td class="border-b-2 border-gray-200 px-4 py-3">{{ item.total }}</td>
-                                </tr>
-                              </tbody>
-                            </table>
-                          </div>
                     </div>
                 </div>
             </div>
